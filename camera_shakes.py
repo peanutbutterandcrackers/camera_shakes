d3594ae85309cbd866d3597ec48746b10a59a750
Prafulla Giri <pratheblackdiamond@gmail.com>
2019-01-23 16:17:25 +0545

    (UI) Enhancements: Allow for the plugin parameters to be set from within GIMP.

--- camera_shakes.py
+++ camera_shakes.py
@@ -1,37 +1,22 @@
 #!/usr/bin/env python
 
-# Subtle shake:
-# Horizontal and vertical movement range [-5, 5]
-# Rotation Range [-0.5, 0.5]
-
 from gimpfu import *
 import easy_easer, pytweening
 import math, random
 
-xOffsetLowerBound = -5
-xOffsetUpperBound = 5
-yOffsetLowerBound = -5*2
-yOffsetUpperBound = 5*2
-
-rotationLowerBound = -0.5*2
-rotationUpperBound = 0.1*2
-
-xOffsetMidPoint = (xOffsetLowerBound + xOffsetUpperBound) / 2
-yOffsetMidPoint = (yOffsetUpperBound + yOffsetUpperBound) / 2
-rotationMid = (rotationLowerBound + rotationUpperBound ) / 2
-
-xOffSigma = 1.5
-yOffSigma = 1.25
-rotSigma  = 0.125
-
-def camera_shake(image, drawable, total_keyframes, in_betweens):
+def camera_shake(image, drawable, total_keyframes, in_betweens, xOffsetLowerBound, xOffsetUpperBound,
+  yOffsetLowerBound, yOffsetUpperBound, rotationLowerBound, rotationUpperBound, xOffSigma,
+    yOffSigma, rotSigma):
 	pdb.gimp_image_undo_group_start(image)
 
 	shakee = pdb.gimp_image_get_active_layer(image) # the layer that will be shaked
+	
+	xOffsetMidPoint = (xOffsetLowerBound + xOffsetUpperBound) / 2
+	yOffsetMidPoint = (yOffsetUpperBound + yOffsetUpperBound) / 2
+	rotationMid = (rotationLowerBound + rotationUpperBound ) / 2
 
 	total_frames = (in_betweens + 1) * (total_keyframes - 1) + 1
 	frames_done = 0
-
 	pdb.gimp_message("Total Animation Frames: %s" % total_frames)
 
 	while frames_done < total_frames:
@@ -78,7 +63,16 @@ register(
 		(PF_IMAGE, "image", "Takes current Image", None),
 		(PF_DRAWABLE, "drawable", "Input Layer", None),
 		(PF_INT, "total_keyframes", "Camera Shake Keyframes", 50),
-		(PF_INT, "in_betweens", "number of frames in between camera shake keyframes", 2)
+		(PF_INT, "in_betweens", "Keyframe Inbetweens", 2),
+		(PF_FLOAT, "xOffsetLowerBound", "X-Offset Lower Bound", -5),
+		(PF_FLOAT, "xOffsetUpperBound", "X-Offset Upper Bound", 5),
+		(PF_FLOAT, "yOffsetLowerBound", "Y-Offset Lower Bound", -5*2),
+		(PF_FLOAT, "yOffsetUpperBound", "Y-Offset Upper Bound", 5*2),
+		(PF_FLOAT, "rotationLowerBound", "Rotation Lower Bound", -0.5*2),
+		(PF_FLOAT, "rotationUpperBound", "Rotation Upper Bound", 0.1*2),
+		(PF_FLOAT, "xOffSigma", "X Offset Sigma", 1.5),
+		(PF_FLOAT, "yOffSigma", "Y Offset Sigma", 1.25),
+		(PF_FLOAT, "rotSigma", "Rotation Sigma", 0.125)
         ],
         [],
         camera_shake,
