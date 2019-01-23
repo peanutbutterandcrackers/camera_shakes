9f28f6552b3845ad1d6b54ac266f453575bf0d6c
Prafulla Giri <pratheblackdiamond@gmail.com>
2019-01-23 09:23:21 +0545

    Remove unnecessary lines of code; switch to saner sigma values

--- camera_shakes.py
+++ camera_shakes.py
@@ -19,9 +19,9 @@ xOffsetMidPoint = (xOffsetLowerBound + xOffsetUpperBound) / 2
 yOffsetMidPoint = (yOffsetUpperBound + yOffsetUpperBound) / 2
 rotationMid = (rotationLowerBound + rotationUpperBound ) / 2
 
-xOffSigma = 1 
-yOffSigma = 1 
-rotSigma  = 1 
+xOffSigma = 1.5
+yOffSigma = 1.25
+rotSigma  = 0.125
 
 def camera_shake(image, drawable, total_frames):
 	pdb.gimp_image_undo_group_start(image)
@@ -30,14 +30,6 @@ def camera_shake(image, drawable, total_frames):
 
 	frames_done = 0
 	while frames_done != total_frames:
-
-		xOffsetLow = random.triangular(int(xOffsetLowerBound), int(xOffsetMidPoint))
-		xOffsetHi = random.triangular(int(xOffsetMidPoint), int(xOffsetUpperBound))
-		yOffsetLow = random.triangular(int(yOffsetLowerBound), int(yOffsetMidPoint))
-		yOffsetHi = random.triangular(int(yOffsetMidPoint), int(yOffsetUpperBound))
-		rotLow = random.triangular(int(rotationLowerBound), int(rotationMid))
-		rotHi = random.triangular(int(rotationMid), int(rotationUpperBound))
-
 		new_frame = pdb.gimp_layer_copy(shakee, TRUE)
 		pdb.gimp_image_insert_layer(image, new_frame, None, 0)
 
@@ -45,7 +37,6 @@ def camera_shake(image, drawable, total_frames):
 		pdb.gimp_drawable_offset(new_frame, FALSE, OFFSET_TRANSPARENT, random.gauss(xOffsetMidPoint, xOffSigma), random.gauss(yOffsetMidPoint, yOffSigma))
 		# random rotation on every single frame
 		pdb.gimp_item_transform_rotate(new_frame, math.radians(random.gauss(rotationMid, rotSigma)), TRUE, 0, 0)
-
 
 		# TODO: Merge new_frame down with previous layer, remove new_frame and set the merged_layer as the new_frame
 		# This will prevent the final image sequence from having different borders, brings about uniformity and the illusion works better
